cf6a743dfa3895c23be6d137447573bee6dd18e3
Aldaaairr <aldaair10@gmail.com>
2022-10-18 01:06:29 -0500

    Login API

---
+++ Usuario/apiLogin/src/main/java/com/example/demo/models/UsuarioModel.java
@@ -0,0 +1,54 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    
+    
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+
+    
+
+   
+    
+}
